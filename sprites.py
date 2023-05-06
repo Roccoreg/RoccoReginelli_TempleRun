f52ad94cba7aca2000e522f238ec56930f968549
Roccoreg <rocco.reginelli23@bcp.org>
2023-05-06 14:07:20 -0700

    1

--- sprites.py
+++ sprites.py
@@ -1,24 +1,29 @@
 # File created by Rocco Reginelli
 import pygame as pg
 from settings import *
+vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprites):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((30, 40))
         self.image.fill(YELLOW)
-        self.rect = self.image.get_rect
-        self.vx = 0
-        self.vy = 0
+        self.rect = self.image.get_rect()
+        self.rec.center = (WIDTH / 2, HEIGHT / 2)
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
 
 def update(self):
-    self.vx = 0
-    keys = pg.keys.get_pressed()
+    self.acc = vec(0, 0)
+    keys = pg.key.get_pressed()
     if keys[pg.K_LEFT]:
-        self.vx = -5
+        self.acc.x = -0.5
     if keys[pg.K_RIGHT]:
-        self.vx = 5
+        self.acc.x = 0.5
 
-    self.rect.x += self.vx
-    self.rect.y += self.vy
+    self.vel += self.acc
+    self.pos += self.vel + 0.5 * self.acc
+
+    self.rect.center = self.pos
 
