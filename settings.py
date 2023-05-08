456bfff671a981581c45fd5edc386513e66349da
Roccoreg <rocco.reginelli23@bcp.org>
2023-05-08 10:58:21 -0700

    comit

--- settings.py
+++ settings.py
@@ -7,6 +7,13 @@ FPS = 60
 # Payer properties
 PLAYER_ACC = 0.5
 PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+# starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
+                 (125, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 20)]
 
 # define colors
 WHITE = (255, 255, 255)
@@ -15,3 +22,4 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
